615812b92c545f7fe283b20632f989c270daf0a9
student-air <230972@students.au.edu.pk>
2026-07-11 22:14:15 +0500

    initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('ApartMate'),
         ),
       ),
     );
