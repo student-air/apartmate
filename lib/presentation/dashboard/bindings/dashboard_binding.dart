e1f53d9e242ae98cd975f11f2048ac288effdbb7
student-air <230972@students.au.edu.pk>
2026-07-22 05:38:52 +0500

    Final Dashboard

--- lib/presentation/dashboard/bindings/dashboard_binding.dart
+++ lib/presentation/dashboard/bindings/dashboard_binding.dart
@@ -1,20 +1,3 @@
-// import 'package:get/get.dart';
-// import 'package:apartmate/domain/repositories/i_auth_repository.dart';
-// import 'package:apartmate/domain/repositories/i_society_repository.dart';
-// import 'package:apartmate/presentation/dashboard/controllers/dashboard_controller.dart';
-
-// class DashboardBinding extends Bindings {
-//   @override
-//   void dependencies() {
-//     Get.lazyPut<DashboardController>(
-//       () => DashboardController(
-//         Get.find<ISocietyRepository>(),
-//         Get.find<IAuthRepository>(),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:get/get.dart';
 import 'package:apartmate/domain/repositories/i_dashboard_repository.dart';
 import 'package:apartmate/domain/repositories/i_society_repository.dart';
